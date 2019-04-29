d3396a7b4540ec45edd0ea96ba82488a4cacd4dc
ValentinDutin <valentin.dutin@gmail.com>
2019-04-29 14:57:14 +0200

    added task4

---
+++ UI/task_4/posts.js
@@ -0,0 +1,282 @@
+var photoPosts = [
+    {
+       id: "1",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "2",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "3",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "4",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "5",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "6",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "7",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "8",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "9",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "10",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "11",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "12",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "13",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "14",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "15",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "16",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "17",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "18",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "19",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+    {
+        id: "20",
+        description: "Лето где-то рядом",
+        createdAt: new Date("2018-02-23T23:00:00"),
+        author: "valentin_dutin",
+        photoLink: "https://pp.userapi.com/c854220/v854220904/2973e/lWxDJEnXnr8.jpg",
+        hashtag: ["summer", "minsk", "belinsta"],
+    },
+
+];
+
+(function() {
+function compareByDate(first, second){
+    if(first.createdAt > second.createdAt){
+        return -1;
+    }
+    else if(first.createdAt > second.createdAt){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+function getPhotoPosts(skip){
+    return getPhotoPosts(skip, 10);
+}
+function getPhotoPosts(){
+    return getPhotoPosts(0, 10);
+}
+function getPhotoPost(id){
+    photoPosts.sort(compareByDate).foreach(function(item){
+        if(item.id == id){
+            return item;
+        }
+    });
+}
+
+function getPhotoPosts(skip, top){
+    if(skip < 0){
+        skip = 0;
+    }
+    if(top < 0){
+        top = 10;
+    }
+    if(skip + top >= photoPosts.length){
+        top = photoPosts.size() - skip;
+    }
+    resultArr = [];
+    return photoPosts.sort(compareByDate).slice(skip, top + skip);
+}
+
+function getPhotoPosts(skip, top, filterConfig){
+    if(skip < 0){
+        skip = 0;
+    }
+    if(top < 0){
+        top = 10;
+    }
+    if(skip + top >= photoPosts.length){
+        top = photoPosts.size() - skip;
+    }
+    resultArr = [];
+    var count = 0;
+    photoPosts.sort(compareByDate).foreach(function(item){
+        if(item.createdAt == filterConfig.createdAt ||
+        item.author == filterConfig.author){
+            resultArr.push(item);
+        }
+        for(i = 0; i < item.hashtag.length; k++){
+            if(item.hashtag[i] == filterConfig.hashtag){
+                resultArr.push(item);
+                break;
+            }
+        }
+    });
+    return resultArr.slice(skip, top + skip);
+}
+function validatePhotoPost(photoPost){
+    if(photoPost.id == null){
+    console.log("Error!");
+        return false;
+    }
+    if(photoPost.description == null ||
+    photoPost.description.length >= 200){
+        return false;
+    }
+    if(photoPost.author == null){
+        return false;
+    }
+    if(photoPost.createdAt == null){
+        return false;
+    }
+    if(photoPost.photoLink == null){
+        return false;
+    }
+    return true;
+}
+function addPhotoPost(photoPost){
+    if(validatePhotoPost(photoPost)){
+        photoPosts.push(photoPost);
+    }
+}
+function removePhotoPost(id){
+    for(i = 0;i < photoPosts.length; ++i){
+        if(photoPosts[i].id == id){
+        alert(photoPosts[i].author );
+        photoPosts.splice(i, 1);
+        }
+    }
+}
+function editPhotoPost(id, photoPost){
+    post = getPhotoPost(id);
+    if(photoPost.descriprion != null ||
+    photoPost.descriprion.length < 200){
+        post.descriprion = photoPost.descriprion;
+    }
+    if(photoPost.photoLink != null){
+        post.photoLink = photoPost.photoLink;
+    }
+    if(photoPost.likes != null){
+
+        post.likes = photoPost.likes;
+    }
+    if(photoPost.hashtag != null){
+        post.hashtag = photoPost.hashtag;
+    }
+}
+
+}());
